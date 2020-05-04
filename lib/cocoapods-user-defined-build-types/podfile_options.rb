c9077a02f2b79e208b84a1da13a048131f5c92d6
Jonathan Cardasis <joncardasis@gmail.com>
2020-05-04 12:57:13 -0400

    Determine CP version to use correct BuildType namespace

--- lib/cocoapods-user-defined-build-types/podfile_options.rb
+++ lib/cocoapods-user-defined-build-types/podfile_options.rb
@@ -1,3 +1,8 @@
+is_version_1_9_x = Pod.const_defined?(:BuildType) # CP v1.9.x
+
+# Assign BuildType to proper module definition dependent on CP version.
+BuildType = is_version_1_9_x ? Pod::BuildType : Pod::Target::BuildType
+
 module Pod    
   class UserOption
 
@@ -8,10 +13,10 @@ module Pod
     # [Hash{String, BuildType}] mapping of Podfile keyword to a BuildType
     def self.keyword_mapping
       {
-        :dynamic_framework => Pod::Target::BuildType.dynamic_framework,
-        :dynamic_library => Pod::Target::BuildType.dynamic_library,
-        :static_framework => Pod::Target::BuildType.static_framework,
-        :static_library => Pod::Target::BuildType.static_library
+        :dynamic_framework => BuildType.dynamic_framework,
+        :dynamic_library => BuildType.dynamic_library,
+        :static_framework => BuildType.static_framework,
+        :static_library => BuildType.static_library
       }
     end
   end
